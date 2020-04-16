bbbc8b6a000a32ee6fea97186e8218d2444a5ebf
daisuke0131 <dai.tachikoma@gmail.com>
2020-04-16 09:42:41 +0900

    first commit

---
+++ sample/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Greeting />
+  </div>
+</template>
+
+<script>
+import Greeting from "./components/Greeting.vue";
+
+export default {
+  name: "App",
+  components: {
+    Greeting
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ sample/tests/unit/Greeting.spec.js
@@ -0,0 +1,11 @@
+import { mount } from "@vue/test-utils";
+import Greeting from "@/components/Greeting.vue";
+
+describe("Greeting.vue", () => {
+  it("renders a greeting", () => {
+    const wrapper = mount(Greeting);
+
+    console.log(wrapper.html());
+    expect(wrapper.text()).toMatch("Vue and TDD");
+  });
+});
