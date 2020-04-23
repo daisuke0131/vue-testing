b9e891179e09088e109b36ae26852c1d38c7ca8f
daisuke0131 <dai.tachikoma@gmail.com>
2020-04-24 01:40:52 +0900

    Stubbing components

---
+++ sample/tests/unit/ParentWithAPICallChild.spec.js
@@ -0,0 +1,18 @@
+import { shallowMount,mount } from "@vue/test-utils";
+import ParentWithAPICallChild from "@/components/ParentWithAPICallChild.vue";
+import ComponentWithAsyncCall from "@/components/ComponentWithAsyncCall.vue";
+
+describe("ParentWithAPICallChild.vue", () => {
+  it("renders with mount and does initialize API call", () => {
+    // const wrapper = mount(ParentWithAPICallChild, {
+    //   stubs: {
+    //     ComponentWithAsyncCall: "<div class='stub'></div>"
+    //   }
+    // });
+
+    const wrapper = shallowMount(ParentWithAPICallChild)
+
+    console.log(wrapper.html());
+    expect(wrapper.find(ComponentWithAsyncCall).exists()).toBe(true);
+  });
+});
