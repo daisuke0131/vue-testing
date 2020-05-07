662538917e2cf80a7e4366bf2d5c0a99c1c0fad2
daisuke0131 <dai.tachikoma@gmail.com>
2020-05-07 09:20:53 +0900

    Testing Vuex in components

---
+++ sample/src/components/ComponentWithGetters.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="fullname">{{ fullname }}</div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+// export default {
+//   name: "ComponentWithGetters",
+
+//   computed: {
+//     fullname() {
+//       return this.$store.getters.fullname;
+//     }
+//   }
+// };
+
+export default {
+  name: "ComponentWithGetters",
+
+  computed: {
+    ...mapGetters(["fullname"])
+  }
+};
+</script>
---
+++ sample/tests/unit/ComponentWithGetters.spec.js
@@ -0,0 +1,47 @@
+import Vuex from "vuex";
+import { mount, createLocalVue } from "@vue/test-utils";
+import ComponentWithGetters from "@/components/ComponentWithGetters.vue";
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    firstName: "Alice",
+    lastName: "Doe"
+  },
+
+  getters: {
+    fullname: state => state.firstName + " " + state.lastName
+  }
+});
+
+it("renders a username using a real Vuex getter", () => {
+  const wrapper = mount(ComponentWithGetters, { store, localVue });
+
+  expect(wrapper.find(".fullname").text()).toBe("Alice Doe");
+});
+
+it("renders a username using computed mounting options", () => {
+  const wrapper = mount(ComponentWithGetters, {
+    mocks: {
+      $store: {
+        getters: {
+          fullname: "Alice Doe"
+        }
+      }
+    }
+  });
+
+  expect(wrapper.find(".fullname").text()).toBe("Alice Doe");
+});
+
+it("renders a username using computed mounting options", () => {
+  const wrapper = mount(ComponentWithGetters, {
+    computed: {
+      fullname: () => "Alice Doe"
+    }
+  });
+
+  expect(wrapper.find(".fullname").text()).toBe("Alice Doe");
+});
