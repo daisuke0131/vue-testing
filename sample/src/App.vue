1a85997343129979df4ad49f7b7ba7083e4dea8e
daisuke0131 <dai.tachikoma@gmail.com>
2020-05-11 10:57:10 +0900

    router testing

--- sample/src/App.vue
+++ sample/src/App.vue
@@ -1,27 +1,11 @@
 <template>
   <div id="app">
-    <Greeting />
+    <router-view />
   </div>
 </template>
 
 <script>
-import Greeting from "./components/Greeting.vue";
-
 export default {
-  name: "App",
-  components: {
-    Greeting
-  }
+  name: "app"
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
